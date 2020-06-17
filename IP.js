8c503481f2cd581dc7eaf4c67cf1d61099abc769
Orz-3 <61905744+Orz-3@users.noreply.github.com>
2020-06-18 07:45:58 +0800

    Add files via upload

---
+++ IP.js
@@ -0,0 +1,46 @@
+if ($response.statusCode != 200) {
+  $done(null);
+}
+
+const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
+var city0 = "高谭市";
+var isp0 = "Cross-GFW.org";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function City_ValidCheck(para) {
+  if(para) {
+  return para
+  } else
+  {
+  return city0
+//emojis[getRandomInt(emojis.length)]
+  }
+}
+
+function ISP_ValidCheck(para) {
+  if(para) {
+  return para
+  } else
+  {
+  return isp0
+//emojis[getRandomInt(emojis.length)]
+  }
+}
+
+function Area_check(para) {
+  if(para=="中华民国"){
+  return "台湾"
+  } else
+  {
+  return para
+  }
+}
+var body = $response.body;
+var obj = JSON.parse(body);
+var title =Area_check(obj['country']);
+var subtitle =City_ValidCheck(obj['city'])+'-'+'('+ ISP_ValidCheck(obj['org'])+')';
+var ip = obj['query'];
+var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
+$done({title, subtitle, ip, description});
---
+++ README.md
@@ -0,0 +1,14 @@
+# QuantumultX
+
+自用QuantumultX规则及脚本，收集整理自以下dalao
+
+@ConnersHua 神机规则
+
+@lhie1 复写规则，由NobyDa整理
+
+@KOP-XIAO IP-API，资源转换器
+
+@NobyDa 贴吧，吾爱，爱奇艺签到等
+
+@chavyleung WPS签到
+
